92b58ac084351289eafa90248ee92801824a2a17
Gus <gustavo.gbalbino@hotmail.com>
2022-05-26 20:32:23 -0300

    26/05

---
+++ Funções/Notes01.js
@@ -0,0 +1,34 @@
+// Função em JS é First-Class object (Citizens) - Higher order function
+
+/*
+function fun1() {}
+
+const fun2 = function () {} //Armanzenar em variável
+
+const array = [function (a, b) {return a + b}] //Armazenar em array
+
+console.log(array[0](2,3))
+
+const obj = {} //Armazenar em objeto
+obj.falar = function() {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parâmetro
+function execute(fun3) {
+    fun3()
+}
+
+execute(function () {console.log('Executando....')})
+
+//Uma função ela pode retornar/conter uma função
+
+function soma (a, b) {
+    return function (c) {
+        console.log(a + b+c)
+    }
+}
+
+const cincoTot = soma(2,3)
+cincoTot(4)
+*/
+
